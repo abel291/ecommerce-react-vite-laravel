f2f4d7643fd1235cd9ef9e8398a0840891233b24
Abel <abel291m@gmail.com>
2024-09-09 05:05:17 -0500

    fix image get id front

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -15,6 +15,7 @@ class ImageResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'img' => $this->img,
             'alt' => $this->alt,
             'type' => $this->type,
